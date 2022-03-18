e58fdd5ce67802ab0172fb3f62efd921954ed529
Robert Nitsch <mail@robertnitsch.de>
2022-03-18 20:25:51 +0100

    Set version to 1.1.0

--- src/py3createtorrent.py
+++ src/py3createtorrent.py
@@ -48,7 +48,7 @@ __all__ = ['calculate_piece_length', 'get_files_in_directory', 'sha1', 'split_pa
 
 # Do not touch anything below this line unless you know what you're doing!
 
-__version__ = '1.0.1'
+__version__ = '1.1.0'
 
 # Note:
 #  Kilobyte = kB  = 1000 Bytes
