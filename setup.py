0206f63c99389314e2ad039de1acc6d5582c79b5
Robert Nitsch <mail@robertnitsch.de>
2024-02-13 17:09:16 +0100

    Add Python 3.11 and 3.12 to supported versions

--- setup.py
+++ setup.py
@@ -52,6 +52,8 @@ setup(
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3 :: Only",
     ],
     # This field adds keywords for your project which will appear on the
